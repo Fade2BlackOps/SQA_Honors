f0a6e75fe3d313896313accd99eeb9807c7030b0
Vasil Ivanov <vtivanoviii5757@eagle.fgcu.edu>
2025-03-20 15:50:41 -0400

    Added Customer class

---
+++ src/classes/Customer.java
@@ -0,0 +1,30 @@
+package classes;
+
+public class Customer  extends Person {
+
+    // Constructor
+    // ----------
+    public Customer() {} // empty constructor
+
+    public Customer(String name, String phoneNumber, String email, String address) {
+        super(name, phoneNumber, email, address); // construct Person
+    }
+
+    // Methods
+    // -------------
+
+    /**
+     * Simulates buying a book.
+     */
+    public void buyBook(Book book) {
+        System.out.println(getName() + " has bought a book.");
+    }
+
+    /**
+     * Simulates returning a book.
+     */
+    public void returnBook() {
+        System.out.println(getName() + " has returned a book.");
+    }
+
+}
