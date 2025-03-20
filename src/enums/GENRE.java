8a90772400c51c0bef2d710342f0ab4f6a0c239c
Vasil Ivanov <vtivanoviii5757@eagle.fgcu.edu>
2025-03-20 16:34:27 -0400

    Added GENRE enumerator for book genres

---
+++ src/enums/GENRE.java
@@ -0,0 +1,16 @@
+package enums;
+
+public enum GENRE {
+    FICTION,
+    FANTASY,
+    ROMANCE,
+    ACTION,
+    MYSTERY,
+    SCIENCE_FICTION,
+    THRILLER,
+    HORROR,
+    POETRY,
+    NON_FICTION,
+    GRAPHIC_NOVEL,
+    BIOGRAPHY
+}
