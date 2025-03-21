6674e836e674d36bcaea63b5c6b5e4c2772aec41
Vasil Ivanov <vtivanoviii5757@eagle.fgcu.edu>
2025-03-21 10:11:27 -0400

    Added Author class

---
+++ src/classes/Author.java
@@ -0,0 +1,37 @@
+package classes;
+
+public class Author extends Person {
+
+    // Constructor
+    // -------
+    public Author() {}
+
+    public Author(String name, String phoneNumber, String email, String address) {
+        super(name, phoneNumber, email, address);
+    }
+
+    // Methods
+    // --------
+
+    /**
+     * Simulates an Author writing a Book
+     *
+     * @return book = the new book that was written
+     */
+    public Book writeBook() {
+        System.out.println("Author" + getName() + "is writing a book.");
+        Book book = new Book();
+        // TODO: Fill in the rest of the logic for a book being written.
+        return book;
+    }
+
+    /**
+     * Simulates an Author revising a Book
+     */
+    public void reviseBook(Book book) {
+        System.out.println("Author" + getName() + "is revising a book.");
+        // book.openBook(edit mode)
+        // Book should open in author's default word-processing app
+    }
+
+}
