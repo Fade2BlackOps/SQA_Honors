23adacb6accda14e758880fb2cfe7bb05d942899
VTIvanovIII <vtivanoviii5757@eagle.fgcu.edu>
2025-03-21 12:11:06 -0400

    Added EBook class

---
+++ src/classes/EBook.java
@@ -0,0 +1,29 @@
+package classes;
+
+import enums.GENRE;
+
+public class EBook extends Book {
+    // Private attributes for encapsulation
+    // -------------------------------------------------------------------------
+    private String fileFormat; // e.g., PDF, EPUB, MOBI
+    private float fileSize; // in MB
+
+    // Constructors
+    // ----------------------
+    public EBook() {}
+
+    public EBook(String title, String isbn, String author, String publisher, int pageCount, float price, GENRE genre, String fileFormat, float fileSize) {
+        super(title, isbn, author, publisher, pageCount, price, genre);
+        this.fileFormat = fileFormat;
+        this.fileSize = fileSize;
+    }
+
+    // Getters and Setters
+    // --------
+    public String getFileFormat() { return fileFormat; }
+    public void setFileFormat(String fileFormat) { this.fileFormat = fileFormat; }
+
+    public float getFileSize() { return fileSize; }
+    public void setFileSize(float fileSize) { this.fileSize = fileSize;}
+    
+}
