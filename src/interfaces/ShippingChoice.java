db8629f139765dfc099fbb48b4349071f8bdf709
VTIvanovIII <vtivanoviii5757@eagle.fgcu.edu>
2025-03-21 15:47:20 -0400

    Updated ShippingChoice interface methods

--- src/interfaces/ShippingChoice.java
+++ src/interfaces/ShippingChoice.java
@@ -3,17 +3,18 @@ package interfaces;
 public interface ShippingChoice {
 
     /**
-     * Simulates a shipping choice for a book.
-     * 
-     * @return the shipping method chosen
+     * Simulates the process of shipping an order.
+     * This method would typically involve updating the order status and
+     * notifying the customer about the shipping details.
      */
-    String chooseShippingMethod();
+    void shipOrder();
 
     /**
      * Simulates a shipping cost calculation based on the chosen method.
      * 
      * @param method the shipping method chosen
+     * 
      * @return the calculated shipping cost
      */
-    float calculateShippingCost(String method);
+    float getShippingCost(String method); // TODO: // Change to enum for shipping methods
 }
