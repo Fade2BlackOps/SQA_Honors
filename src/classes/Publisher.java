889e0a0960b4e398818c0700a22068da60a1f433
VTIvanovIII <vtivanoviii5757@eagle.fgcu.edu>
2025-03-21 12:06:58 -0400

    Added methods to Publisher class

--- src/classes/Publisher.java
+++ src/classes/Publisher.java
@@ -17,6 +17,29 @@ public class Publisher extends Person {
      * Simulates a Publisher editing a book
      * Publishers can suggest edits to a book
      */
+    public void editBook(Book book) {
+        System.out.println("Publisher " + getName() + " is editing a book.");
+        // book.openBook("edit"); // Open the book in edit mode
+    }
 
+    /**
+     * Simulates a Publisher publishing a book
+     * Publishers can publish a book
+     */
+    public void publishBook(Book book) {
+        System.out.println("Publisher " + getName() + " is publishing a book.");
+        // List book for sale in database
+        // book.publish(); // Publish the book
+    }
+
+    /**
+     * Publisher can set the wholesale price of a book
+     * @param book the book to set the price for
+     * @param price the wholesale price to set
+     */
+    public void setWholesalePrice(Book book, float price) {
+        System.out.println("Publisher " + getName() + " is setting the wholesale price of the book to $" + price);
+        book.setPrice(price); // Set the wholesale price of the book
+    }
 
 }
