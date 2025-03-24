c4d75f874ec858b89f24c00b69ebb50ba791b161
VTIvanovIII <vtivanoviii5757@eagle.fgcu.edu>
2025-03-24 10:21:04 -0400

    Changed paymentMethod to enumerator

--- src/classes/Payment.java
+++ src/classes/Payment.java
@@ -1,27 +1,27 @@
 package classes;
 
+import enums.PAYMENT_TYPE;
+
 public class Payment {
 
     // Private attributes for encapsulation
     // -------------------------------------------------------------------------
-    private String paymentMethod; // e.g., Credit Card, PayPal
-    // TODO: Change to enum for better type safety
-    // private PaymentMethod paymentMethod; // Enum for payment methods
-    private float amount; // Amount to be paid
+    private PAYMENT_TYPE paymentMethod;
+    private float amount;
 
     // Constructors
     // ----------------------
     public Payment() {}
 
-    public Payment(String paymentMethod, float amount) {
+    public Payment(PAYMENT_TYPE paymentMethod, float amount) {
         this.paymentMethod = paymentMethod;
         this.amount = amount;
     }
 
     // Getters and Setters
     // --------
-    public String getPaymentMethod() { return paymentMethod; }
-    public void setPaymentMethod(String paymentMethod) { this.paymentMethod = paymentMethod; }
+    public PAYMENT_TYPE getPaymentMethod() { return paymentMethod; }
+    public void setPaymentMethod(PAYMENT_TYPE paymentMethod) { this.paymentMethod = paymentMethod; }
 
     public float getAmount() { return amount; }
     public void setAmount(float amount) { this.amount = amount; }
