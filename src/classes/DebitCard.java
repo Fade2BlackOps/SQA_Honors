530b36ae92c916054aa45ab59aa9e664f1c2fe7c
VTIvanovIII <vtivanoviii5757@eagle.fgcu.edu>
2025-03-24 14:33:50 -0400

    Add DebitCard class for enhanced payment processing functionality

---
+++ src/classes/DebitCard.java
@@ -0,0 +1,35 @@
+package classes;
+
+import enums.PAYMENT_TYPE;
+
+public class DebitCard extends Card {
+    private String bank;
+
+    public DebitCard() {
+        super(); // Call the default constructor of Card
+    }
+
+    public DebitCard(float paymentAmount, String userName, String cardNumber, String bank) {
+        super(paymentAmount, userName, cardNumber);
+        this.bank = bank;
+    }
+
+    public DebitCard(int orderId, PAYMENT_TYPE paymentMethod, float amount, String cardNumber, String cardHolderName, String expirationDate, String cvv, String bank) {
+        super(orderId, paymentMethod, amount, cardNumber, cardHolderName, expirationDate, cvv); // Call the parameterized constructor of Card
+        this.bank = bank;
+    }
+
+    public String getBank() { return bank; }
+    public void setBank(String bank) { this.bank = bank; }
+
+
+    @Override
+    public Payment processPayment(int orderId, PAYMENT_TYPE paymentMethod, float amount) {
+        System.out.println("Processing " + paymentMethod + " payment of $" + amount);
+        System.out.println("Using bank: " + bank);
+        Payment payment = new Cash(orderId, paymentMethod, amount);
+        // Simulate payment processing logic here
+        return payment; // Assume payment is always successful for simplicity
+    }
+
+}
